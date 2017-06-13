6ba4cd8fa66179c9fc710855cd3dfce1f73af103
PhantomUnicorns <skyrim.ultimate.masters@gmail.com>
2017-06-13 19:36:09 -0400

    Created base meteorstriketimer.java
    Created basic form of meteor strike timer task

---
+++ src/mortuusterra/timers/MeteorStrikeTimer.java
@@ -0,0 +1,21 @@
+package mortuusterra.timers;
+
+import mortuusterra.Main;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class MeteorStrikeTimer implements BukkitRunnable {
+	
+	Main plugin;
+
+	public MeteorStrikeTimer(Main plugin) {
+		this.plugin = plugin;
+		run();
+	}
+
+	public void run() {
+		// TODO
+		
+	}
+
+}
