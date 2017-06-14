6d9209bf4fb414e3aad7c7e6ff6faf0638c15f37
PhantomUnicorns <skyrim.ultimate.masters@gmail.com>
2017-06-13 21:13:02 -0400

    Added createMeteor method
    This method creates a meteor somewhere near the base according to the radius!

--- src/mortuusterra/timers/MeteorStrikeTimer.java
+++ src/mortuusterra/timers/MeteorStrikeTimer.java
@@ -2,7 +2,13 @@ package mortuusterra.timers;
 
 import mortuusterra.Main;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 public class MeteorStrikeTimer implements BukkitRunnable {
 	
@@ -16,6 +22,27 @@ public class MeteorStrikeTimer implements BukkitRunnable {
 	public void run() {
 		// TODO
 		
+	}
+	
+	public FallingBlock createMeteor(Location base, int radius) {
+		World world = base.getWorld();
+		double xLocation = (Math.random() * (radius * 2)) - radius;
+		double yLocation = base.getY() + 255;
+		double zLocation = (Math.random() * (radius * 2)) - radius;
+		Location local = base.clone().add(xLocation, yLocation, zLocation);
+
+		FallingBlock meteor = (FallingBlock) world.spawnFallingBlock(local, Material.ENDER_STONE, (byte) 0);
+		
+		double xVector = (Math.random() * 0.8) - 0.4;
+		double yVector = -3 + (Math.random() * 0.4);
+		double zVector = (Math.random() * 0.8) - 0.4;
+		Vector vector = new Vector(xVector, yVector, zVector);
+		
+		meteor.setVelocity(vector);
+		meteor.setCustomName("METEOR");
+		meteor.setDropItem(false);
+		meteor.setHurtEntities(true);
+		return meteor;
 	}
 
 }
