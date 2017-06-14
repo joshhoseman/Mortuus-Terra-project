0d08d3e22d4ba78a7d99e221b4ec4e144c48413d
PhantomUnicorns <skyrim.ultimate.masters@gmail.com>
2017-06-13 21:49:37 -0400

    Added another listener
    Added EntityChangeBlockEvent

--- src/mortuusterra/listeners/EntityListener.java
+++ src/mortuusterra/listeners/EntityListener.java
@@ -3,9 +3,9 @@ package mortuusterra.listeners;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 
 import mortuusterra.timers.MeteorStrikeTimer;
 import mortuusterra.Main;
@@ -20,10 +20,18 @@ public class BlockFormListener implements Listener {
 	
 	@EventHandler
 	public void playerDestroysBlock(EntityDamageByEntityEvent event) {
-    if (event.getDamager() instanceof FallingBlock && event.getEntity() instanceof Player
+    		if (event.getDamager() instanceof FallingBlock && event.getEntity() instanceof Player
 				&& event.getDamager().getCustomName().equals(MeteorStrikeTimer.meteorName)) {
 			Player player = (Player) event.getEntity();
 			event.setDamage(player.getHealth() * 0.9);
+		}
+	}
+	
+	@EventHandler
+	public void playerDestroysBlock(EntityChangeBlockEvent event) {
+    		if (event.getEntity() instanceof FallingBlock && event.getEntity().getCustomName().equals(MeteorStrikeTimer.meteorName)) {
+			event.getBlock().setType(Material.IRON_ORE);
+			event.setCancelled(true);
 		}
 	}
 }
