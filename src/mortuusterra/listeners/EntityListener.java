a81bb559fde732a59839d286d4d23f37df054fd7
PhantomUnicorns <skyrim.ultimate.masters@gmail.com>
2017-06-14 12:42:38 -0400

    Added worldguard support
    Added worldguard support; Made it so it won't damage or form blocks for a meteor strike happening in protected area.

--- src/mortuusterra/listeners/EntityListener.java
+++ src/mortuusterra/listeners/EntityListener.java
@@ -1,5 +1,8 @@
 package mortuusterra.listeners;
 
+import java.util.Map;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Player;
@@ -8,32 +11,68 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 
+import com.sk89q.worldguard.bukkit.WGBukkit;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+
 import mortuusterra.timers.MeteorStrikeTimer;
 import mortuusterra.Main;
 
+@SuppressWarnings("unused")
 public class EntityListener implements Listener {
+	
+	private final Main plugin;
+	
 	public EntityListener(Main plugin) {
 		this.plugin = plugin;
 	}
-
-	@SuppressWarnings("unused")
-	private final Main plugin;
-	
-	
 	
 	@EventHandler
 	public void entityDamageEntityEvent(EntityDamageByEntityEvent event) {
     		if (event.getDamager() instanceof FallingBlock && event.getEntity() instanceof Player
 				&& event.getDamager().getCustomName().equals(MeteorStrikeTimer.meteorName)) {
 			Player player = (Player) event.getEntity();
-			event.setDamage(player.getHealth() * 0.9);
+			Map<String, ProtectedRegion> regions = WGBukkit.getRegionManager(player.getWorld()).getRegions();
+			boolean inRegion = false;
+
+			Location local = player.getLocation();
+			int x = ((int) local.getX());
+			int y = ((int) local.getY());
+			int z = ((int) local.getZ());
+
+			for (ProtectedRegion region : regions.values()) {
+				if (region.contains(x, y, z)) {
+					inRegion = true;
+				}
+			}
+			if (!inRegion) {
+				event.setDamage(player.getHealth() * 0.9);
+			} else {
+				event.setDamage(0);	
+			}
 		}
 	}
 	
 	@EventHandler
-	public void entityChangeBlockEvent(EntityChangeBlockEvent event) {
-    		if (event.getEntity() instanceof FallingBlock && event.getEntity().getCustomName().equals(MeteorStrikeTimer.meteorName)) {
-			event.getBlock().setType(Material.IRON_ORE);
+	public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+		if (event.getEntity() instanceof FallingBlock && event.getEntity().getCustomName().equals("METEOR")) {
+			Map<String, ProtectedRegion> regions = WGBukkit.getRegionManager(event.getEntity().getWorld()).getRegions();
+			boolean inRegion = false;
+
+			Location local = event.getEntity().getLocation();
+			int x = ((int) local.getX());
+			int y = ((int) local.getY());
+			int z = ((int) local.getZ());
+
+			for (ProtectedRegion region : regions.values()) {
+				if (region.contains(x, y, z)) {
+					inRegion = true;
+				}
+			}
+			if (!inRegion) {
+				event.getBlock().setType(Material.IRON_ORE);
+			} else {
+				event.getBlock().setType(Material.AIR);	
+			}
 			event.setCancelled(true);
 		}
 	}
